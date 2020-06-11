c9861d93c63ede76de4227673cbde30f9fae7c68
Nicholas Myers <nicholas.myers.porfessional@gmail.com>
2020-06-11 08:46:53 -0700

    set up the Customer Controller

--- src/main/java/com/lambdaschool/orders/controllers/CustomerController.java
+++ src/main/java/com/lambdaschool/orders/controllers/CustomerController.java
@@ -1,12 +1,28 @@
 package com.lambdaschool.orders.controllers;
 
+import com.lambdaschool.orders.models.Customer;
+import com.lambdaschool.orders.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController
 {
+   @Autowired
+   private CustomerService customerService;
 
+   @GetMapping(value = "/customers",
+   produces = {"application/json"})
+   public ResponseEntity<?> listAllRestaurants()
+   {
+      List<Customer> allCustomers = customerService.findAllCustomers();
+      (allCustomers, HttpStatus.OK);
+   }
 }
